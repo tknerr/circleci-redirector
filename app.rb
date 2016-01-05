94a7c1d557e0dbe6a0d48c67a144e39b822af67a
Torben Knerr <mail@tknerr.de>
2016-01-06 00:30:03 +0100

    add more TODOs

--- app.rb
+++ app.rb
@@ -10,9 +10,15 @@ get '/todo' do
       * show readme or routes on /
       * forward accept headers and pass thru content type
       * access by build_num not only latest
+      * list branches (from github?)
       * list builds per branch
+      * add new-relic or other ping service to prevent idling
       * add tests
       * auto-deploy to openshift
+      * use a custom domain name
+      * use 302 redirect for all /* GET requests so it behaves more like a drop-in proxy
+      * also forward auth tokens and allow https only
+      * add donate button
   EOF
 end
 
